5144a4195b8b59b23f2b7901cff4b2f74bf48ce1
jenkinsFD <91998827+jenkinsFD@users.noreply.github.com>
2021-10-07 18:17:56 -0500

    Update ls_file.js

--- ls_file.js
+++ ls_file.js
@@ -8,12 +8,22 @@ fs.readdir("./files_csv", function (err, archivos) {
 	}
 console.log(archivos);
 	
-	archivos.forEach(function(file){
-		console.log(file);
-		stats = fs.statSync(file);
-		console.log(stats.mtime);
-		console.log(stats.ctime);
-	});
+
 });
 
+const fs = require("fs");
+const path = require("path");
 
+const getMostRecentFile = (dir) => {
+  const files = orderReccentFiles(dir);
+  return files.length ? files[0] : undefined;
+};
+
+const orderReccentFiles = (dir) => {
+  return fs.readdirSync(dir)
+    .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
+    .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
+    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+};
+
+console.log(getMostRecentFile('./files_csv'));
